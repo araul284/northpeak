57dbfc18d931657ff62bb8b0a3c7279822d7ab32
araul284 <arpitapaul22ap@gmail.com>
2026-07-25 14:48:06 +0530

    feat: add scroll animation and interactive click for navigation cards

--- src/App.tsx
+++ src/App.tsx
@@ -43,8 +43,6 @@ export default function App() {
 
   return (
     <div className="min-h-screen bg-white text-zinc-900 font-sans selection:bg-black selection:text-white antialiased overflow-x-hidden">
-      {/* Scroll Progress Indicator */}
-
       {/* Sticky Navigation Bar */}
       <Navbar
         onNavigate={handleNavigate}
